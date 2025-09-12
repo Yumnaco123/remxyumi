a5307d8224d21ea7636303b02ff06374455c338e
Yumnaco123 <ranimahrani830@gmail.com>
2025-09-12 11:07:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = ""; // Ganti dengan domain utama kalian
-const serviceName = ""; // Ganti dengan nama workers kalian
+const rootDomain = "mhrani29.my.id"; // Ganti dengan domain utama kalian
+const serviceName = "simplyyurie"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
